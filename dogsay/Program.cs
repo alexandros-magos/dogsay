0d48c01e83c813f974e4290c376b33fb1d6e8b88
AlexandrosOMagos <v.alexandros@proton.me>
2026-08-15 16:15:06 +0300

    Add project files.

---
+++ dogsay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dogsay_main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What do you want the dog to say?");
+            string textInput = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"   __\r\no-''|\\_____/)\r\n \\_/|_)     )\r\n    \\  __  /\r\n    (_/ (_/    {textInput}\r\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("===============\rPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
